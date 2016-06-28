1b93e6bbdfaf07c6f574f4f5e76a83854cbe14a0
cyrilpanicker <cyrilpanicker@gmail.com>
2016-06-28 16:40:42 +0530

    added changes

--- source/server/logging-service.ts
+++ source/server/logging-service.ts
@@ -1,12 +1,49 @@
 import * as moment from 'moment';
+import * as winston from 'winston';
 
-export const logMessage = (message:string) => {
-    const stamp = moment(new Date()).format('MM/DD HH:mm');
-    console.log(stamp+' | '+message);
-}
+const timestamp = () => {
+    return moment(new Date()).format('MM/DD HH:mm:ss');
+};
+const formatter = options => {
+    return options.timestamp()+' | '+options.level.toUpperCase()+' | '+options.message;
+};
 
-export const logProcessMessage = ({id,stock,count,error,url}) => {
-    const stamp = moment(new Date()).format('MM/DD HH:mm');
-    const message = stamp+' | '+id+' | '+stock+' | '+count+' | '+error+' | '+url;
-    console.log(message);
-}
+const exceptionLogger = (()=>{
+    return new winston.Logger({
+        transports:[
+            new winston.transports.File({
+                filename:'./logs/exceptions.log',
+                timestamp,
+                handleExceptions:true,
+                humanReadableUnhandledException:true,
+                json:false,
+                formatter:options => {
+                    return options.timestamp()+' | '+options.level.toUpperCase()+' | '+options.meta.stack.join('\n');
+                }
+            })
+        ]
+    });
+})();
+
+export const functionalLogger = (()=>{
+    return new winston.Logger({
+        transports:[
+            // new winston.transports.Console({timestamp,formatter})
+            new winston.transports.File({filename:'./logs/functional.log',timestamp,formatter,json:false})
+        ]
+    });
+})();
+
+
+const processLogger = (()=>{
+    return new winston.Logger({
+        transports:[
+            // new winston.transports.Console({timestamp,formatter})
+            new winston.transports.File({filename:'./logs/process.log',timestamp,formatter,json:false})
+        ]
+    });
+})();
+
+export const logProcessedInfo = ({id,stock,count,lastDate,error,url}) => {
+    processLogger.info(id+' | '+stock+' | '+count+' | '+lastDate+' | '+error+' | '+url);
+};
--- source/server/main.ts
+++ source/server/main.ts
@@ -1,60 +1,70 @@
 import * as express from 'express';
 import {delay,readFile} from './util-services';
-import {logMessage,logProcessMessage} from './logging-service';
+import {functionalLogger,logProcessedInfo} from './logging-service';
 import {getCandleData,getCandleDataUrl} from './yahoo-service';
 
 let stocksList = [];
 let stockPointer = 0;
 
-readFile('./data/stocks-list.test.json').then(
-    (data:string) => {
-        stocksList = JSON.parse(data);
-        logMessage('processing started')
-        processStocks();
-    },
-    error => logMessage(error)
-);
+console.log(process.env.NODE_ENV);
+functionalLogger.error('Hi | message');
+logProcessedInfo({id:1,count:123,error:null,stock:'INFY',lastDate:'08-06-2016',url:'asdkg.com'});
+console.log(stocksList[9].ads);
 
-const processStocks = () => {
-    const url = getCandleDataUrl({stock:stocksList[stockPointer].symbol,endDate:new Date()});
-    getCandleData({
-        stock:stocksList[stockPointer].symbol,
-        endDate:new Date()
-    }).then(
-        (candles:any[]) => {
-            logProcessMessage({
-                id:stockPointer,
-                stock:stocksList[stockPointer].symbol,
-                count:candles.length,
-                error:null,
-                url
-            });
-        },
-        error => {
-            logProcessMessage({
-                id:stockPointer,
-                stock:stocksList[stockPointer].symbol,
-                count:null,
-                error:error,
-                url
-            });
-        }
-    ).then(
-        () => {
-            stockPointer++;
-            if(stockPointer < stocksList.length){
-                return delay(20000);
-            }else{
-                return delay(0);
-            }
-        }
-    ).then(
-        () => {
-            if(stockPointer < stocksList.length){
-                processStocks();
-            }else{
-                logMessage('processing finished');
-            }
-        }
-    )
-};
+// const mode = process.env.NODE_ENV;
+// const stocksListFile = mode === 'PROD' ? './data/stocks-list.json' : './data/stocks-list.test.json';
+
+// readFile(stocksListFile).then(
+//     (data:string) => {
+//         stocksList = JSON.parse(data);
+//         logMessage('processing started')
+//         processStocks();
+//     },
+//     error => logMessage(error)
+// );
+
+// const processStocks = () => {
+//     const url = getCandleDataUrl({stock:stocksList[stockPointer].symbol,endDate:new Date()});
+//     getCandleData({
+//         stock:stocksList[stockPointer].symbol,
+//         endDate:new Date()
+//     }).then(
+//         (candles:any[]) => {
+//             logProcessMessage({
+//                 id:stockPointer,
+//                 stock:stocksList[stockPointer].symbol,
+//                 count:candles.length,
+//                 lastDate:candles[candles.length-1].date,
+//                 error:null,
+//                 url
+//             });
+//         },
+//         error => {
+//             logProcessMessage({
+//                 id:stockPointer,
+//                 stock:stocksList[stockPointer].symbol,
+//                 count:null,
+//                 lastDate:null,
+//                 error:error,
+//                 url
+//             });
+//         }
+//     ).then(
+//         () => {
+//             stockPointer++;
+//             if(stockPointer < stocksList.length){
+//                 return delay(20000);
+//             }else{
+//                 return delay(0);
+//             }
+//         }
+//     ).then(
+//         () => {
+//             if(stockPointer < stocksList.length){
+//                 processStocks();
+//             }else{
+//                 logMessage('processing finished');
+//             }
+//         }
+//     )
+// };
